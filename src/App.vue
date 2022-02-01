6f30a88e32159b60fb31bbb020a94406204fdfb1
lukasganster <lukasganster13@gmail.com>
2022-02-01 19:49:39 +0100

    Refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,5 @@
 <template>
-  <Provider v-bind:rssUrl="url1" :maxArticles="10" />
-  <Provider v-bind:rssUrl="url2" :maxArticles="10" />
-  <Provider v-bind:rssUrl="url3" :maxArticles="10" />
+  <router-view />
 </template>
 
-<script>
-import Provider from "./components/Provider.vue";
-
-export default {
-  name: "App",
-  components: {
-    Provider,
-  },
-  data() {
-    return {
-      url1: "https://rss.orf.at/news.xml",
-      url2: "https://www.diepresse.com/rss/home",
-      url3: "https://tvthek.orf.at/feed/rss",
-    };
-  },
-};
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="less"></style>
