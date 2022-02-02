a7c526be3718d289bfe40f028944339de51d61e1
lukasganster <lukasganster13@gmail.com>
2022-02-02 14:52:41 +0100

    Add pwa

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/projects/News2/" : "/",
+  pwa: {
+    name: "News",
+    themeColor: "#2e3439",
+    display: "fullscreen",
+  },
 };
