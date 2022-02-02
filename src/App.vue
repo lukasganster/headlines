9fec8face50a70346b0628b589cfda804bb2bac0
lukasganster <lukasganster13@gmail.com>
2022-02-02 14:48:17 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,25 @@
 <template>
-  <router-view />
+  <transition name="fade" mode="out-in">
+    <keep-alive>
+      <router-view />
+    </keep-alive>
+  </transition>
 </template>
 
-<style lang="less"></style>
+<style>
+@import url("http://fonts.cdnfonts.com/css/sf-ui-display");
+a {
+  text-decoration: none;
+  cursor: pointer;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+</style>
