daa9ae35ed8ef7bd18866b599a2676a6245682bd
lukasganster <lukasganster13@gmail.com>
2022-02-02 14:48:28 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import News from "../views/News.vue";
+import Settings from "../views/Settings.vue";
 
 const routes = [
   {
@@ -7,10 +8,15 @@ const routes = [
     name: "News",
     component: News,
   },
+  {
+    path: "/settings",
+    name: "Einstellungen",
+    component: Settings,
+  },
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
 });
 
